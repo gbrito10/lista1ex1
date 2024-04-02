3768f8b9415f246fba2e0cbbdff91fb3470d1ea7
gbrito10 <165733920+gbrito10@users.noreply.github.com>
2024-04-01 21:10:33 -0300

    Add files via upload

---
+++ lista1ex1/lista1ex1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1ex1
+{
+    internal class Program
+    {       
+        static void Main(string[] args)
+        {
+            int b;
+            int a;
+            int area;
+           
+
+            Console.Write("digite a base do retângulo:");
+            b = int.Parse(Console.ReadLine());
+
+            Console.Write("digite a altura do retângulo:");
+            a = int.Parse(Console.ReadLine());
+
+            area = b * a;
+
+            Console.Write("a área do retângulo é:");
+            Console.WriteLine(area);
+           
+
+
+
+        }
+    }
+}
